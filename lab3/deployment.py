8579f105cafb322ccb64776d1e812a172198933b
AndShelep <andrii.shelep.shi.2023@lpnu.ua>
2024-12-12 20:29:48 +0200

    first commit

---
+++ lab3/deployment.py
@@ -0,0 +1,35 @@
+import os
+
+from django.conf import STATICFILES_STORAGE_ALIAS
+from django.conf.global_settings import STATIC_ROOT
+
+from .settings import *
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
+DEBUG = False
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "hotel_booking_py",
+        'HOST': "hotelbooking-server.mysql.database.azure.com",
+        'USER': "rootShelep",
+        'PASSWORD': "pass@1324",
+    }
+}
